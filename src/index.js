cc1f8d3e08faa65ee42a0c53a54d341608ac00de
George Simos <georgesimos@hotmail.gr>
2018-12-19 12:34:58 +0200

    Finalize the counter app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { createStore } from "redux";
+import Counter from "./components/Counter";
+import counter from "./reducers";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(counter);
+const rootEl = document.getElementById("root");
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const render = () => {
+  ReactDOM.render(
+    <Counter
+      value={store.getState()}
+      onIncrement={() => store.dispatch({ type: "INCREMENT" })}
+      onDecrement={() => store.dispatch({ type: "DECREMENT" })}
+    />,
+    rootEl
+  );
+};
+
+render();
+store.subscribe(render);
